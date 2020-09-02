65c19150ab6909f8823fe9ec1eb31e357cd068db
armando-j-a-santos <47860995+armando-j-a-santos@users.noreply.github.com>
2020-09-02 15:27:39 +0100

    Add files via upload

--- webcomponent.js
+++ webcomponent.js
@@ -5,7 +5,7 @@
     <div id="di-autocomplete-main"
     class="decisionIncComponentWidgetPanel">
         <input id="di-autocomplete-widget" class="decisionIncInputField " value="" key="" type="text"
-            placeholder="Enter Text..."
+            placeholder="..."
             style="font-size: 14px; border-color: rgb(191, 191, 191); 
             background-color: rgb(255, 255, 255);">
 
@@ -13,7 +13,25 @@
 
     <style>
 
-                    
+table, td {
+  border: 1px solid black;
+}    
+      
+<table id="myTable">
+  <tr>
+    <td>Row1 cell1</td>
+    <td>Row1 cell2</td>
+  </tr>
+  <tr>
+    <td>Row2 cell1</td>
+    <td>Row2 cell2</td>
+  </tr>
+  <tr>
+    <td>Row3 cell1</td>
+    <td>Row3 cell2</td>
+  </tr>
+</table>
+          
             * {
                 box-sizing: border-box;
             }
