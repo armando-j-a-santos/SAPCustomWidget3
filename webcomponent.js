49431236831af9de7b23a6e597995edba715b768
armando-j-a-santos <47860995+armando-j-a-santos@users.noreply.github.com>
2020-09-02 15:52:24 +0100

    Add files via upload

--- webcomponent.js
+++ webcomponent.js
@@ -1,15 +1,12 @@
 (function () {
 
-var table = document.createElement('table');
-table.setAttribute('border','1');
-table.setAttribute('width','100%')
-var row = table.insertRow(0);
-for(j=1; j<=10; j++){
-    var text = document.createTextNode(String.fromCharCode(j+64));
-    var cell = row.insertCell(j-1);
-    cell.setAttribute('align','center')
-    cell.appendChild(text);
+var HTML = "<table border=1 width=100%><tr>";
+for(j=1;j<=10;j++)
+{
+    HTML += "<td align=center>"+String.fromCharCode(j+64)+"</td>";
 }
+HTML += "</tr></table>";
+document.getElementById("outputDiv").innerHTML = HTML;
 
 
 
