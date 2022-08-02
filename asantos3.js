ef236e6f2745167cb1e533c546e431008e5cabd6
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 11:39:33 +0100

    Update asantos3.js

--- asantos3.js
+++ asantos3.js
@@ -82,7 +82,7 @@ div.testColumn4{
     </style>
     `;
 
-    customElements.define('com-demo-asantos3', class DiAutoComplete extends HTMLElement {
+    customElements.define('com-demo-asantos2', class DiAutoComplete extends HTMLElement {
 
 
         constructor() {
