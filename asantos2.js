3c3b6de765d87148c77ac7a290983aaeac9e51d9
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 11:12:21 +0100

    Update asantos2.js

--- asantos2.js
+++ asantos2.js
@@ -82,7 +82,7 @@ div.testColumn4{
     </style>
     `;
 
-    customElements.define('com-sap-autoarmando1001', class DiAutoComplete extends HTMLElement {
+    customElements.define('com-demo-pepe', class DiAutoComplete extends HTMLElement {
 
 
         constructor() {
